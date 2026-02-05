e74588223b69127e9da447b5c4437f4e67a249bd
EdwardGal <edwgit@gmail.com>
2026-02-05 12:54:08 +0300

    date in js

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,61 @@
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { createElement, Fragment } from "react";
 
 function App() {
-	//императивный стиль
-	//явно указыаем как получить год
 	const currentYear = new Date().getFullYear();
 
-	//декларативный стиль
-	//возвращаем jsx в котором явно указываем что мы хотим видеть
-	return (
-		<>
-			<div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img
-						src={reactLogo}
-						className="logo react"
-						alt="React logo"
-					/>
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			{currentYear}
-			<div className="card">
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-		</>
+	const logos = createElement(
+		"div",
+		null,
+		createElement(
+			"a",
+			{ href: "https://vite.dev", target: "_blank" },
+			createElement("img", {
+				src: viteLogo,
+				className: "logo",
+				alt: "Vite logo",
+			}),
+		),
+		createElement(
+			"a",
+			{ href: "https://react.dev", target: "_blank" },
+			createElement("img", {
+				src: reactLogo,
+				className: "logo",
+				alt: "React logo",
+			}),
+		),
 	);
+
+	const title = createElement("h1", null, null);
+
+	const card = createElement(
+		"div",
+		{ className: "card" },
+		createElement(
+			"p",
+			null,
+			"Edit <code>src/App.jsx</code> and save to test HMR",
+		),
+	);
+
+	const docs = createElement(
+		"div",
+		{ className: "read-the-docs" },
+		"Click on the Vite and React logos to learn more",
+	);
+
+	const app = createElement(Fragment, null, [
+		logos,
+		title,
+		currentYear,
+		card,
+		docs,
+	]);
+
+	return app;
 }
 
 export default App;
